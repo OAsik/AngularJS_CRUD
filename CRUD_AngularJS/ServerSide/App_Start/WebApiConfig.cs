0bbf08c00c08fc399e5e800b6af0209f33882283
Özgür <z.ozgur.a@gmail.com>
2018-03-23 23:49:03 +0300

    Nice and easy tutorial on AngularJS and ASP .NET Web API
    Dynamic data entry on table with AngularJS on client-side. Database has been established on server-side web API project. Dynamic HTML response and parallax effect with CSS.

---
+++ CRUD_AngularJS/ServerSide/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ServerSide
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var cors = new System.Web.Http.Cors.EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CRUD_AngularJS/ServerSide/Controllers/ShipperController.cs
@@ -0,0 +1,91 @@
+﻿using ServerSide.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ServerSide.Controllers
+{
+    public class ShipperController : ApiController
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public IHttpActionResult GetShipper()
+        {
+            List<Shippers> shipperList = db.Shippers.ToList();
+            return Json(shipperList);
+        }
+
+        [ResponseType(typeof(Shippers))]
+        public IHttpActionResult GetShipper(int id)
+        {
+            Shippers shipper = db.Shippers.Find(id);
+            if (shipper == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(shipper);
+            }
+        }
+
+        [ResponseType(typeof(Shippers))]
+        public IHttpActionResult DeleteShipper(int id)
+        {
+            Shippers shipper = db.Shippers.Find(id);
+            if (shipper == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Shippers.Remove(shipper);
+                db.SaveChanges();
+                return Json(db.Shippers.ToList());
+            }
+        }
+
+        [ResponseType(typeof(Shippers))]
+        public IHttpActionResult PutShipper(Shippers shipper)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                db.Entry(shipper).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+
+            }
+
+            return Json(db.Shippers.ToList());
+        }
+
+        [ResponseType(typeof(Shippers))]
+        public IHttpActionResult PostShipper(Shippers shipper)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                db.Shippers.Add(shipper);
+                db.SaveChanges();
+                return Json(db.Shippers.ToList());
+            }
+        }
+    }
+}
